99e076a65ddbbca103711fa7180896c6f21339a1
Richard Bartoš <ricci21@google.com>
2022-12-02 09:53:59 +0100

    Style fixes, refactor variable names, refactor counting of calories of top 3 elfs.

--- day01/day01.py
+++ day01/day01.py
@@ -1,27 +1,29 @@
 import os.path
 
-#setup list of sums for each elf
-mapElfCalories = []
+# setup list of sums for each elf
+list_elf_calories = []
 
 with open(os.path.join(os.path.dirname(__file__), 'data.txt')) as fh:
+    one_elf_calories = []
 
-    singleElfCalories = []
-    
     for line in fh.readlines():
         line = line.strip()
-        
+
         if line == "":
-            mapElfCalories.append(sum(singleElfCalories))
-            singleElfCalories = []
+            list_elf_calories.append(sum(one_elf_calories))
+            one_elf_calories = []
         else:
-            singleElfCalories.append(int(line))
-    
-    #Add the last elf to the map as the "" is missing on the last line 
-    mapElfCalories.append(sum(singleElfCalories))
+            one_elf_calories.append(int(line))
 
-print("Elf with most calories:")
-print(max(mapElfCalories))
+    # Add the last elf to the map as the "" is missing on the last line
+    list_elf_calories.append(sum(one_elf_calories))
 
-print("Top 3 with most calories:")
-mapElfCalories.sort(reverse=True)
-print(mapElfCalories[0]+mapElfCalories[1]+mapElfCalories[2])
+print("Number of calories carried by the most packed elf:")
+print(max(list_elf_calories))
+
+print("Number of calories of the three most packed elfs:")
+if len(list_elf_calories) < 3:
+    print("There are less than 3 elfs!")
+else:
+    list_elf_calories.sort(reverse=True)
+    print(sum(list_elf_calories[0:2]))
